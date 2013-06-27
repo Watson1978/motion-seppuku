ec8cac1dbebd442f2874bd5c2a3aee2c4d85f2c2
Watson <watson1978@gmail.com>
2013-06-28 00:51:43 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# motion-seppuku
+
+Unistall RubyMotion (A sample of RubyMotion Command-Line Plugin).
---
+++ command/motion-seppuku.rb
@@ -0,0 +1,17 @@
+require 'optparse'
+
+module Motion; module Project
+  class SeppukuCommand < Command
+    self.name = 'seppuku'
+    self.help = 'Uninstall RubyMotion'
+
+    def run(args)
+      system "cp /Library/RubyMotion/license.key ~/Desktop"
+
+      system "sudo rm -rf ~/Library/RubyMotion"
+      system "sudo rm -rf /Library/RubyMotion"
+      system "sudo rm -rf /usr/bin/motion"
+    end
+
+  end
+end; end
